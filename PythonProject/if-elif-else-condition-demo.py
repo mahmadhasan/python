715d962f80392ac5cd844d291fa11eaaa87150ea
=mahmadhasanbabaawsdevops <=mahmadhasanbabaawsdevops@gmail.com>
2026-08-13 16:50:22 +0530

    added nested if examples

--- PythonProject/if-elif-else-condition-demo.py
+++ PythonProject/if-elif-else-condition-demo.py
@@ -23,4 +23,31 @@ elif marks>=35:
 else:
     print("Fail")
 
+#Nested If statement
+#username = "admin"
+#pwd ="admin123"
+#role = "trainer"
+username = input("Enter the username")
+pwd = input("Enter the password")
+role = input("Enter the role")
+if username == "admin" and pwd =="admin123":
+    if role == "student":
+       print("display student dashboard")
+    elif role == "trainer":
+        print("display trainer dashboard")
+    elif role == "admin":
+        print("display admin dashboard")
+else :
+    print("Invalid credentials")
 
+#Nested if for Withdraw_amount
+balance = 5000
+withdraw_amount = int(input("Enter the amount to withdraw"))
+if balance>0:
+    if withdraw_amount <= balance:
+        balance = balance - withdraw_amount
+        print("Withdraw success and remaining balance",balance)
+    else :
+        print("Insufficient balance")
+else :
+        print("Funds not available")
