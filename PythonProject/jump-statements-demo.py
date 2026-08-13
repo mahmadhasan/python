f79758e11baf4d483cd54a5c8f7538678b59ef9e
=mahmadhasanbabaawsdevops <=mahmadhasanbabaawsdevops@gmail.com>
2026-08-13 20:21:14 +0530

    added jumping statements

---
+++ PythonProject/jump-statements-demo.py
@@ -0,0 +1,13 @@
+for i in range(1,10):
+    if i == 5:
+        break
+    print(i)
+
+#search name
+students =["baba","john","javeed","hasan","gangadhar"]
+search_name="javeed"
+for student in students:
+    print("Checking..",student)
+    if search_name == student:
+        print("student found..",student)
+        break
