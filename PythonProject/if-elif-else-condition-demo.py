583e257c4013f2a13aeb8d5f644fd722c59d8f24
=mahmadhasanbabaawsdevops <=mahmadhasanbabaawsdevops@gmail.com>
2026-08-13 16:11:18 +0530

    conditional statements -if-elif-else

---
+++ PythonProject/if-elif-else-condition-demo.py
@@ -0,0 +1,26 @@
+age = 12
+if age>=18:
+    print("Eligible to vote..")
+
+else :
+     print("Not eligible for vote")
+
+#Multiple conditions use if-elif-else statements
+#marks = 100
+marks = int(input("Enter the Marks"))
+if marks>=90:
+    print("Grade-A")
+elif marks>=80:
+    print("Grade-B")
+elif marks>=70:
+    print("Grade-C")
+elif marks>=50:
+    print("Grade-D")
+elif marks>=45:
+    print("Grade-E")
+elif marks>=35:
+    print("pass")
+else:
+    print("Fail")
+
+
